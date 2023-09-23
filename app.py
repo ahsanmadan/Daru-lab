ffd44d4e7142a09cba27014a459a3f284d940536
Ahsan Ramadan <ahsanramadan07@gmail.com>
2023-09-23 12:23:52 +0700

    env

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, jsonify, request
+from flask import (
+    Flask, render_template, jsonify, request, session, redirect, url_for
+)
 from pymongo import MongoClient
+import jwt
 from datetime import datetime
 
-client = MongoClient("mongodb+srv://test:asan@cluster0.hmhssac.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient(
+    "mongodb+srv://test:asan@cluster0.hmhssac.mongodb.net/?retryWrites=true&w=majority")
 db = client.daru
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('index.html')    
+    return render_template('index.html')
