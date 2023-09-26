34c4827324b9f2ca4b2f5e776af9915ec54eef65
Aqsyaaurora <aqsyaaurora29@gmail.com>
2023-09-26 11:31:01 +0700

    update

--- app.py
+++ app.py
@@ -19,9 +19,40 @@ def home():
 def faq():
     return render_template('FAQs.html')
     
-@app.route('/product')
+@app.route('/produk')
 def produk():
     return render_template('Produk.html')
+
+@app.route('/us')
+def us():
+    return render_template('us.html')
+
+@app.route('/dampak')
+def dampak():
+    return render_template('dampak.html')
+
+@app.route('/produk1')
+def produk1():
+    return render_template ('detailProduk1.html')
+
+@app.route('/produk2')
+def produk2():
+    return render_template ('detailProduk2.html')
+
+@app.route('/produk3')
+def produk3():
+    return render_template ('detailProduk3.html')
+
+@app.route('/produk4')
+def produk4():
+    return render_template ('detailProduk4.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
 
 
 if __name__ == '__main__':
