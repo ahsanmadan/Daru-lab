5072ef4f9c8c889a97ee734c458b1674b0608120
Aqsyaaurora <aqsyaaurora29@gmail.com>
2023-09-27 13:23:40 +0700

    update toast

--- static/js/pagemain.js
+++ static/js/pagemain.js
@@ -31,3 +31,15 @@ href=""
 ></a>
 </div>
   `;
+
+
+    const toastTrigger = document.getElementById("liveToastBtn");
+    const toastLiveExample = document.getElementById("liveToast");
+
+    if (toastTrigger) {
+      const toastBootstrap =
+        bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+      toastTrigger.addEventListener("click", () => {
+        toastBootstrap.show();
+      });
+    }
