fc1ed33ca6834cd933e79a21179fe9d3545182ee
Ahsan Ramadan <ahsanramadan07@gmail.com>
2023-09-27 13:35:42 +0700

    pagemain js

--- static/js/pagemain.js
+++ static/js/pagemain.js
@@ -31,3 +31,56 @@ href=""
 ></a>
 </div>
   `;
+
+const foot = `<div class="container-fluid footer py-4">
+<div class="container text-white">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="col-md-6 text-center">
+        <!-- <img src="static/img/map.png" alt="" class="my-3 img-fluid" /> -->
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.297888390594!2d112.7915740745021!3d-7.320397330597662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa96c78410c7%3A0xbe4cb24e572c77a1!2sJl.%20Medokan%20Asri%20Utara%20XIV%20No.35%2C%20Medokan%20Ayu%2C%20Kec.%20Rungkut%2C%20Surabaya%2C%20Jawa%20Timur%2060295!5e0!3m2!1sid!2sid!4v1695084264087!5m2!1sid!2sid"
+          width="600"
+          height="450"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          class="my-3 img-fluid rounded mx-2"
+        ></iframe>
+      </div>
+      <div class="col-md-6"></div>
+      <p class="fs-7 mt-3">
+        <img src="static/img/logo.png" class="me-2" alt="" height="40px" />
+        Copyright ©2023 – Daru Lab. – All rights reserved.
+      </p>
+    </div>
+    <div class="col-md-6 mt-5">
+      <div class="row">
+        <div class="col-md-3 d-flex flex-column fs-14">
+          <h4 class="text-secondary fs-5 fw-bold">Produk</h4>
+          <a href="" class="footer-link">Semua Produk</a>
+        </div>
+        <div class="col-md-3 d-flex flex-column fs-14">
+          <h4 class="text-secondary fs-5 fw-bold">Tentang</h4>
+          <a href="" class="footer-link">Dampak</a>
+          <a href="" class="footer-link">Tentang Kami</a>
+          <a href="" class="footer-link">FAQ</a>
+        </div>
+        <div class="col-md-6 d-flex flex-column fs-14">
+          <h4 class="text-secondary fs-5 fw-bold">Kontak</h4>
+          <a class="footer-link">+62 812-1656-1016</a>
+          <a class="footer-link mb-3">daru.lab@gmail.com</a>
+          <a
+            href="https://www.google.com/maps?ll=-7.320413,112.794149&z=15&t=m&hl=id&gl=ID&mapclient=embed&q=Jl.+Medokan+Asri+Utara+XIV+No.35+Medokan+Ayu+Kec.+Rungkut+Surabaya,+Jawa+Timur+60295"
+            class="footer-link"
+            >Jl. Medokan Asri Utara XIV No.35. Medokan Ayu, Kec. Rungkut,
+            Kota Surabaya, Jawa Timur 602950</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- akhir contact -->`;
