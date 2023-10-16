765690b7e3be4a926fc707e536cefdd384226a5c
Ahsan Ramadan <ahsanramadan07@gmail.com>
2023-10-16 11:37:47 +0700

    80%

---
+++ static/js/daru/home.js
@@ -0,0 +1,17 @@
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("black", "shadow");
+  } else {
+    $("nav").removeClass("black", "shadow");
+  }
+});
+
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
