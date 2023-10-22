135e0459a4c45f2d189304116ebd47b5de81160c
Ahsan Ramadan <ahsanramadan07@gmail.com>
2023-10-22 23:30:02 +0700

    last

--- DaruLab-backup/static/js/daru/home.js
+++
@@ -1,9 +0,0 @@
-var nav = document.querySelector("nav");
-
-window.addEventListener("scroll", function () {
-  if (this.window.pageYOffset > 100) {
-    nav.classList.add("transnav", "shadow");
-  } else {
-    nav.classList.remove("transnav", "shadow");
-  }
-});
